03ba5fbb59e75616f494bada0ee3106c5ea23143
mkcin <marcin_kurowski@yahoo.pl>
2020-04-26 22:14:06 +0200

    cleaning

--- cmake-build-debug/CMakeFiles/pw_zaliczenie2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pw_zaliczenie2.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mkcin/CLionProjects/pw_zaliczenie2
-# Build directory: /home/mkcin/CLionProjects/pw_zaliczenie2/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
